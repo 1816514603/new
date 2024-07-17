30cb894463e17f9a751f240a2fc729207fabfa9a
zhige <1816514603@qq.com>
2024-07-17 09:32:30 +0800

    second

---
+++ app/src/main/java/com/fuish/test2/MainActivity.java
@@ -0,0 +1,108 @@
+package com.fuish.test2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.fuish.test2.fragment.CarFragment;
+import com.fuish.test2.fragment.HomeFragment;
+import com.fuish.test2.fragment.MineFragment;
+import com.fuish.test2.fragment.orderFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private HomeFragment mHomeFragment;
+    private CarFragment mCarFragment;
+    private orderFragment mOrderFragment;
+    private MineFragment mMineFragment;
+    private BottomNavigationView mbottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //初始化控件
+         mbottomNavigationView=findViewById(R.id.bottomNavigationView);
+         mbottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+             @Override
+             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                 if (item.getItemId()==R.id.home){
+              SelectedFragment(0);
+                 }
+                 else if (item.getItemId()==R.id.car) {
+                     SelectedFragment(1);
+                 } else if (item.getItemId()==R.id.order) {
+                     SelectedFragment(2);
+                 }
+                 else {
+                     SelectedFragment(3);
+                 }
+                 return true;
+             }
+         });
+
+         //默认首页选中
+        SelectedFragment(0);
+
+    }
+    private  void SelectedFragment(int position) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        HideFragment(fragmentTransaction);
+        if (position == 0) {
+            if (mHomeFragment == null) {
+                mHomeFragment = new HomeFragment();
+                fragmentTransaction.add(R.id.content, mHomeFragment);
+            } else {
+                fragmentTransaction.show(mHomeFragment);
+            }}
+            else if (position == 1) {
+                if (mOrderFragment == null) {
+                    mOrderFragment = new orderFragment();
+                    fragmentTransaction.add(R.id.content, mOrderFragment);
+                }else {
+                    fragmentTransaction.show(mOrderFragment);
+                }
+            } else if (position==2) {
+                if (mCarFragment==null){
+                    mCarFragment=new CarFragment();
+                    fragmentTransaction.add(R.id.content,mCarFragment);
+                }
+                else {
+                    if (mMineFragment==null){
+                        mMineFragment=new MineFragment();
+                        fragmentTransaction.add(R.id.content,mMineFragment);
+                    }
+                    else{
+                        fragmentTransaction.show(mMineFragment);
+                    }
+                }
+
+        }
+
+            fragmentTransaction.commit();
+
+    }
+
+
+    private void HideFragment(FragmentTransaction fragmentTransaction){
+
+        if (mHomeFragment!=null){
+         fragmentTransaction.hide(mHomeFragment);
+        }
+        if (mOrderFragment!=null){
+            fragmentTransaction.hide(mOrderFragment);
+        }
+        if (mCarFragment!=null){
+            fragmentTransaction.hide(mCarFragment);
+        }
+        if (mMineFragment!=null){
+            fragmentTransaction.hide(mMineFragment);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/fuish/test2/loginActivity.java
@@ -0,0 +1,97 @@
+package com.fuish.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.fuish.test2.db.UserDbHelper;
+import com.fuish.test2.entity.UserInfo;
+
+public class loginActivity extends AppCompatActivity {
+    private EditText et_username;
+    private EditText et_password;
+    private SharedPreferences mSharedPreferences;
+    private CheckBox checkbox;
+    private  boolean is_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //初始化控件
+        et_username=findViewById(R.id.et_username);
+        et_password=findViewById(R.id.et_password);
+        checkbox=findViewById(R.id.checkbox);
+        //获取mSharedPreferences
+        mSharedPreferences=getSharedPreferences("user",MODE_PRIVATE);
+//是否勾选了记住密码
+        is_login=mSharedPreferences.getBoolean("is_login",false);
+        if (is_login){
+            String username=mSharedPreferences.getString("username",null);
+            String password=mSharedPreferences.getString("username",null);
+            et_username.setText(username);
+            et_password.setText(password);
+        }
+
+        //点击注册
+        findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(loginActivity.this,registerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=et_username.getText().toString();
+                String password=et_password.getText().toString();
+                if(TextUtils.isEmpty(username)||TextUtils.isEmpty(password)){
+                    Toast.makeText(loginActivity.this, "请输入用户名和密码", Toast.LENGTH_SHORT).show();
+                }else{
+                    UserInfo login= UserDbHelper.getInstance(loginActivity.this).login(username);
+                    Log.d("ss","11");
+                    if(login!=null) {
+                        if (username.equals(login.getUsername()) || password.equals(login.getPassword())) {
+                            //保存是否勾选了记住密码框
+                            SharedPreferences.Editor edit = mSharedPreferences.edit();
+                            edit.putBoolean("is_login",is_login);
+                            edit.putString("username",username);
+                            edit.putString("password",password);
+                            //这句话不能少
+                            edit.commit();
+                            //登录成功
+                            Intent intent = new Intent(loginActivity.this, MainActivity.class);
+                            Toast.makeText(loginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                            Log.d("login","登录成功");
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(loginActivity.this, "用户名或密码不正确", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(loginActivity.this, "该账号未注册", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        //记住密码点击事件
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                is_login=isChecked;
+            }
+        });
+}
+    }
